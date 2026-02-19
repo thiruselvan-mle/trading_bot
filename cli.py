4ca1025a7cb64e241e76998cafa240b211ea0049
thiruselvan-mle <thiruselvan.muthuraman@gmail.com>
2026-02-19 20:48:35 +0530

    final commit

--- cli.py
+++ cli.py
@@ -11,42 +11,45 @@ load_dotenv()
 logger = logging.getLogger(__name__)
 
 def main():
-    setup_login()
-    parser = argparse.ArgumentParser()
+    try:
+        setup_login()
+        parser = argparse.ArgumentParser()
 
-    parser.add_argument("--symbol", required=True)
-    parser.add_argument("--side", required=True)
-    parser.add_argument("--type", required=True)
-    parser.add_argument("--quantity", type=float, required=True)
-    parser.add_argument("--price", type=float)
+        parser.add_argument("--symbol", required=True)
+        parser.add_argument("--side", required=True)
+        parser.add_argument("--type", required=True)
+        parser.add_argument("--quantity", type=float, required=True)
+        parser.add_argument("--price", type=float)
 
-    args = parser.parse_args()
-    symbol = args.symbol
-    side = args.side
-    order_type = args.type
-    quantity = args.quantity
-    price = args.price
+        args = parser.parse_args()
+        symbol = args.symbol
+        side = args.side
+        order_type = args.type
+        quantity = args.quantity
+        price = args.price
 
-    msg, action = validate_inpt(symbol, side, quantity, price)
+        msg, action = validate_inpt(symbol, side, quantity, price)
 
-    if  not action:
-        print(msg)
-
-    else:
-        if order_type.lower() == 'market':
-            place_market_order(symbol, side, quantity)
-                        
-        elif order_type.lower()  == 'limit':
-            if price is None:
-                logger.error("Price Required For LIMIT Order")
-                print("Price Required For LIMIT Order")
-                return
-            
-            place_limit_order(symbol, side, quantity, price)
+        if  not action:
+            print(msg)
 
         else:
-            logger.error("Invalid order type!")
-            print("Invalid order type")
+            if order_type.lower() == 'market':
+                place_market_order(symbol, side, quantity)
+                            
+            elif order_type.lower()  == 'limit':
+                if price is None:
+                    logger.error("Price Required For LIMIT Order")
+                    print("Price Required For LIMIT Order")
+                    return
+                
+                place_limit_order(symbol, side, quantity, price)
 
+            else:
+                logger.error("Invalid order type!")
+                print("Invalid order type")
+    except Exception as e:
+        print(f"Something went wrong. please try again")
+        logger.error(f"Something went wrong {e}")
 if __name__ == "__main__":
     main()
