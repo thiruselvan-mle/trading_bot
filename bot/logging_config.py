8e110ed1f7bb8211a48eb805a70d3fbc07783883
thiruselvan-mle <thiruselvan.muthuraman@gmail.com>
2026-02-19 20:44:22 +0530

    remove

--- bot/logging_config.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-def setup_login():
-    logging.basicConfig(
-        filename="trading.log",
-        level=logging.INFO,
-        format="%(asctime)s - %(levelname)s - %(message)s"
-    )
